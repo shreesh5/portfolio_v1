8189868d51557f2530a596685ef590e0856ff350
Shreesh Nayak <shreeshnayak@gmail.com>
2020-07-31 15:49:13 -0700

    Added projects section

---
+++ src/components/projects.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './projects.css'
+import { Typography } from '@material-ui/core'
+
+const Projects = () => {
+    return (
+        <div className="projects-section" id="projects">
+            <Typography variant="h3">
+                Projects
+            </Typography>
+        </div>
+    )
+}
+
+export default Projects
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import "../components/layout.css"
 import MenuAppBar from '../components/header'
 import Landing from '../components/landing'
 import About from '../components/about'
+import Projects from '../components/projects'
 import Contact from '../components/contact'
 import Footer from '../components/footer'
 
@@ -14,6 +15,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <Landing />
     <About />
+    <Projects />
     <Contact />
     <Footer />
   </>
