fce502ac0c1023315200a5808e04f703221ccb66
Shreesh Nayak <shreeshnayak@gmail.com>
2020-08-10 00:31:56 -0700

    Added custom card component, project images, project json file, form select and grid

---
+++ src/components/Card.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import Img from 'gatsby-image'
+import GitHubIcon from '@material-ui/icons/GitHub';
+import IconButton from '@material-ui/core/IconButton'
+
+const useStyles = makeStyles({
+  root: {
+      width: 350,
+      height: 420,
+        marginLeft: 30,
+        marginBottom: 40,
+        borderColor: 'black',
+        borderWidth: 2,
+    },
+        title: {
+        fontSize: 14,
+    },
+        pos: {
+        marginBottom: 12,
+    },
+    media: {
+        width: "100%",
+        height: "100%",
+    },
+    imgContainer: {
+        width: "100%",
+        height: 210,
+    },
+    cardcontent: {
+        padding: 0,
+        "&:last-child": {
+            paddingBottom: 0
+        }
+    }
+});
+
+export default function OutlinedCard({ title, description, fluid, url }) {
+    
+    const classes = useStyles();
+
+    return (
+        <Card className={classes.root} variant="outlined">
+        <CardActionArea>
+            <CardMedia className={classes.imgContainer} style={{ display: 'flex', justifyContent: 'center', borderBottom: '2px solid black'}}>
+                <Img 
+                    alt="Screenshot of Project"
+                    fluid={fluid}
+                    className={classes.media}
+                />
+            </CardMedia>
+            <CardContent style={{ textAlign: 'left', height: 160, width: "100%", padding: 15 }}>
+            <Typography gutterBottom>
+                {title}
+            </Typography>
+            <Typography color="textSecondary" component="p" style={{fontSize: 14, lineHeight: 'normal'}}>
+                {description}
+            </Typography>
+            </CardContent>
+        </CardActionArea>
+        <CardActions disableSpacing style={{ textAlign: 'left', height: 50, width: "100%"}}>
+            <a
+                href={url}
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <IconButton>
+                <GitHubIcon size="lg" />
+                </IconButton>
+            </a>
+        </CardActions>
+        </Card>
+    );
+}
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,14 +1,118 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './projects.css'
 import { Typography } from '@material-ui/core'
+import Card from './Card'
+import { Grid } from '@material-ui/core'
+import projectList from "../data/projects.json";
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import { graphql, useStaticQuery } from 'gatsby'
+import Box from "@material-ui/core/Box";
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles({
+    gridContainer: {
+        width: "100%",
+        display: "flex",
+        flexWrap: "wrap",
+    },
+    formControl: {
+        minWidth: 200,
+        marginBottom: 50,
+        marginTop: 30
+      },
+      selectEmpty: {
+        marginTop: 2,
+      },
+    root: {
+        textAlign: 'center'
+    }
+});
 
 const Projects = () => {
+
+    const classes = useStyles()
+
+    const [type, setType] = useState('all')
+
+    const filteredProjectList = type === 'all' ? projectList : projectList.filter((project) => {
+        return (
+            project.type === type
+        )
+    })
+
+    const handleChange = (event) => {
+        setType(event.target.value)
+    }
+
+    const data = useStaticQuery(graphql`
+        query ProjectImgsQuery {
+            ProjectImgs: allFile(filter: {relativePath: {regex: "/projects/.*.png/"}}) {
+            edges {
+                node {
+                    relativePath
+                    name
+                    childImageSharp {
+                        fluid(quality:100, maxWidth: 350) {
+                            ...GatsbyImageSharpFluid
+                            ...GatsbyImageSharpFluidLimitPresentationSize
+                        }
+                    }
+                }
+            }
+            }
+        }
+        `)
+    const { edges: projectImgData } = data.ProjectImgs;
+
     return (
+        <Box className={classes.root}>
         <div className="projects-section" id="projects">
             <Typography variant="h3">
                 Projects
             </Typography>
+            <FormControl variant="outlined" className={classes.formControl}>
+                <InputLabel id="demo-simple-select-outlined-label">Type</InputLabel>
+                <Select
+                    labelId="demo-simple-select-outlined-label"
+                    id="demo-simple-select-outlined"
+                    value={type}
+                    onChange={handleChange}
+                    label="type"
+                    align="center"
+                >
+                <MenuItem value="all">All Projects</MenuItem>
+                <MenuItem value="full-stack">Full Stack</MenuItem>
+                <MenuItem value="machine-learning">Machine Learning</MenuItem>
+                </Select>
+            </FormControl>
+            <Container>
+            <Grid container spacing={3} justify="center" align="center" className={classes.gridContainer}>
+                {
+                    filteredProjectList.map((project) => {
+                        const image = projectImgData.find((n) => {
+                            return n.node.relativePath === `projects/${project.img}`;
+                        });
+                        const imageFluid = image.node.childImageSharp.fluid;
+                        return (
+                            <Grid item xs={12} sm={6} md={4}>
+                                <Card 
+                                    title={project.name}
+                                    fluid={imageFluid}
+                                    description={project.description}
+                                    url={project.url}
+                                />
+                            </Grid>
+                        )
+                    })
+                }
+            </Grid>
+            </Container>
         </div>
+        </Box>
     )
 }
 
