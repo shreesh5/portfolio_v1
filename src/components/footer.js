ce387f15766e320f8d7ed1065d07e500c4477970
Shreesh Nayak <shreeshnayak@gmail.com>
2020-08-16 01:41:40 -0700

    Moved css from .css to .js file for footer section

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,26 @@
 import React from 'react'
-import './footer.css'
+import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 
+const useStyles = makeStyles({
+    footerSection: {
+        textAlign: 'center',
+        fontSize: 16,
+        height: "120px",
+        width: "100%",
+        position: 'absolute',
+        paddingTop: "50px",
+        backgroundColor: '#b71c1c',
+        color: 'white'
+    },
+});
+
 const Footer = () => {
+
+    const classes = useStyles()
+
     return (
-        <div className="footer-section" id="footer">
+        <div className={classes.footerSection} id="footer">
             <div>
                 <Typography>
                     Coded by Shreesh
