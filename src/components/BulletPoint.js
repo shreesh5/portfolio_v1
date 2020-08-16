342af858752957fda5b9eb0b0c39e145f277b034
Shreesh Nayak <shreeshnayak@gmail.com>
2020-08-15 17:43:05 -0700

    Moved bottom bullet point content to json files, removed icon condition from bullet point

--- src/components/BulletPoint.js
+++ src/components/BulletPoint.js
@@ -5,7 +5,7 @@ import iconMap from './Icons'
 const BulletPoint = ({ title, text, icons }) => {
 
     return (
-        <div>
+        <Box border={1} style={{ marginBottom: 25 }}>
             <Box style={{flexDirection: 'row', display: 'flex'}}>
                 { iconMap("outlinedarrow") }
                 <Typography variant="h5" style={{ marginTop: 3, marginLeft:5}}>
@@ -13,25 +13,11 @@ const BulletPoint = ({ title, text, icons }) => {
                 </Typography>
             </Box>
             <Box style={{flexDirection: 'row', display: 'flex', marginLeft: 30}}>
-            { (icons) ?
-                (icons.map(icon => (
-                    <Box style={{flexDirection: 'row', display: 'flex'}}>
-                        <SvgIcon fontSize="small" viewBox="0 0 512 512">
-                            <path d={icon.svg} />
-                        </SvgIcon>
-                        <Typography style={{marginRight: 3, marginLeft: 3}}>
-                            {icon.text}
-                        </Typography>
-                    </Box>
-                )))
-                : (
                     <Typography variant="body1" align="left">
                         {text}
                     </Typography>
-                )
-            }
             </Box>
-        </div>
+        </Box>
     )
 }
 
