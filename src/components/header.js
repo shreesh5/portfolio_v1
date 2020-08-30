542d7cefd45107a8b5ea0d64a6741e8689a7730f
Shreesh Nayak <shreeshnayak@gmail.com>
2020-08-30 12:48:15 -0700

    Updated resume link

--- src/components/header.js
+++ src/components/header.js
@@ -108,7 +108,7 @@ export default function MenuAppBar() {
                             </ListItem>
                             <ListItem>
                               <OutboundLink
-                                href="https://drive.google.com/file/d/1-RPAwiFsqEOplJ-9ruOAXJ_M82FwszQP/view?usp=sharing"
+                                href="https://drive.google.com/file/d/1wwUQMIhAbg8rRc71X52MxDhQVtx04RhN/view?usp=sharing"
                                 target="_blank"
                                 style={{textDecoration: 'none' }}
                               >
@@ -131,7 +131,7 @@ export default function MenuAppBar() {
                         <NavBarLink to="projects" name="Projects"/>
                         <NavBarLink to="contact" name="Contact"/>
                         <OutboundLink
-                          href="https://drive.google.com/file/d/1-RPAwiFsqEOplJ-9ruOAXJ_M82FwszQP/view?usp=sharing"
+                          href="https://drive.google.com/file/d/1wwUQMIhAbg8rRc71X52MxDhQVtx04RhN/view?usp=sharing"
                           target="_blank"
                           style={{textDecoration: "none" }}
                         >
